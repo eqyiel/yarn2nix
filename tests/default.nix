a3ae1a739893948f565fad04bd1b21e25d0a1eaa
Ruben Maher <ruben@maher.fyi>
2018-02-08 12:26:29 +1030

    Bunch of stuff
    - Almost complete rewrite on the JS side with `flow` comment annotations.  Split
      out `yarn2nix.js` into separate modules.
    
    - Fix `sed` hack in `default.nix` by removing the discrepancy between the link
      names in the link farm and what `yarn` expects the names to be in its offline
      cache.  If the names don't match what `yarn` expects, it will contact the
      registry.
    
      See: https://github.com/moretea/yarn2nix/blob/44e16c6e9ea9f5fc28c73ae388342520d95f1529/default.nix#L90-L92
    
    - Drop support for Node < 8.x, which is active LTS since 2017-10-31
    
    - Add a `shell.nix` for development.  This will pull in psuedo-`devDependencies`
      using `node2nix`: `eslint`, `prettier` and `flow`.  They are defined in
      `nix/node/package.json` so that they do not make `yarn.nix` in the root of
      this repo unmaintainable.
    
    - Add `.envrc`.  This just exports a pinned `nixpkgs` on `NIX_PATH` to stay sane
      while developing.
    
    - Add two new zero-dependency dependencies.  These are to help with indentation.
    
    - Use `builtins.readDir` for deciding what tests to run.  This just loops
      through the files of type "directory" in the tests directory, instead of
      specifying each one in `tests/default.nix`.
    
    - Add support for "git+ssh:" dependencies using Nix 2.0's builtins.fetchGit.
      This makes it possible to fetch private git dependencies using the
      `SSH_AUTH_SOCK` of the user doing the building.  Also add tests for this.
    
    - Add support for `file:` and `link:` dependencies (and add tests for these).

--- tests/default.nix
+++ tests/default.nix
@@ -1,10 +1,17 @@
-{ yarn2nix }:
+{ lib, yarn2nix }:
 with builtins;
 let
   build = names: map buildEntry names;
   buildEntry = name: {
     inherit name;
-    value = yarn2nix.mkYarnPackage { src = ./. + "/${name}"; };
+    value = yarn2nix.mkYarnPackage {
+      # This `toPath` call looks redundant, but it's necessary in order to
+      # resolve relative paths in file:// dependencies correctly.
+      src = builtins.toPath (./. + "/${name}");
+    };
   };
 in
-listToAttrs (build ["wetty" "weave-front-end"])
+listToAttrs (build ["yarn2nix-example-a"]# (attrNames
+  # (lib.filterAttrs (name: value: value == "directory")
+  #   (readDir ./.)))
+  )
